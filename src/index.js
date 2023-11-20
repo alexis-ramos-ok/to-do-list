56fe8d38d6fd84898b270d5a8f4a8942341e28cb
Franco Petraglia <frannpetraglia@gmail.com>
2023-11-20 15:54:27 -0300

    agregado-elemento-lista

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Header from './components/Header/Header';
+import List from './components/List/List';
 import Form from './components/Form/Form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <Header />
     <App />
+    <List />
     <Form />
   </React.StrictMode>
 );
