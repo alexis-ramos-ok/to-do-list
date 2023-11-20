241c1962a0856d41f45ae5f0a43dec638fabba27
Franco Petraglia <frannpetraglia@gmail.com>
2023-11-20 19:55:52 -0300

    clock-estatico-agregado

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Form from './components/Form/Form';
+import Clock from './components/Clock/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Header />
+    <Clock />
     <App />
     <List />
     <Form />
