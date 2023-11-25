a9c312d8f19dd0de0c73562cbb22d7ea01765ffa
Daniela <danielamackensy2@gmail.com>
2023-11-24 22:10:46 -0300

    Se agregó estados y funciones para añadir tareas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, {useState} from 'react';
 import './App.css';
-
+import TaskForm from './components/Form/TaskForm';
 
 function App() {
+  
+  const [tasks, setTasks] = useState([]);
+
+  // Agregando nueva tarea
+  const handleAddTask = (taskName) => {
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
+    };
+
+    // Actualizando el estado para incluir la nueva tarea
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+
   return (
+
     <div className="App">
 
+      <TaskForm onAdd={handleAddTask} />
+
+      <ul>
+        {/*Código que mostrará la lista de tareas */}
+        {console.log(tasks)}
+      </ul>
+
     </div>
+
   );
 }
 
